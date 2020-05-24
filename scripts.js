44b601e42b062feb2100e106096b349bd582004a
xangez <xangez348@gmail.com>
2020-05-24 22:16:47 +1000

    added css styling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
 
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1/num2
+}
+
+
+function operate(operator, num1, num2) {
+  if (operator == '+'){
+    add(num1, num2);
+  }
+  else if (operator == '-'){
+    add(num1, num2);
+  }
+  else if (operator == '*'){
+    multiply(num1, num2);
+  }
+  else if (operator == '/'){
+    divide(num1, num2);
+  }
+}
